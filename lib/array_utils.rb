694075072862fd8430c66cc2b7e99a3db3c68f89
Carlos E. Palha <carlao_epalha@hotmail.com>
2015-11-23 13:55:50 -0200

    Arrays - Concluído

--- lib/array_utils.rb
+++ lib/array_utils.rb
@@ -1,4 +1,21 @@
 class ArrayUtils
   def self.multiplos(qtd, multiplo)
+  	arraym = []
+  	for i in 1..qtd
+  		arraym.push(i*multiplo)
+  	end
+  	arraym
+  end
+
+  def self.tabuada(num)
+  	arrayt = []
+  	for i in 1..num
+  		arrayt.push(self.multiplos(10, i))
+  	end
+  	arrayt
+  end
+
+  def self.ultimo(li)
+  	li[-1]
   end
 end
